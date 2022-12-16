e62a783e58a1550321fd5934ce6472ce17ef4066
Uzytkownik <uzytkownik@kursGIT.pl>
2022-12-16 19:13:37 +0100

    sout nowy

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -1,29 +1,12 @@
 package pl.sdacademy;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-
-import javax.persistence.EntityManager;
-
-import static pl.sdacademy.HibernateUtil.getSessionFactory;
-
 /**
  * @author mszyrner
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello jędrna dupa!");
-        Session session = getSessionFactory().openSession();
-
-
-        //Check databse vaersion
-        String sql = "Select version()";
-
-        String result = (String) session.createNativeQuery(sql).getSingleResult();
-        System.out.println(result);
-
-        session.close();
-        HibernateUtil.shutdown();
-
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+  }
 }
