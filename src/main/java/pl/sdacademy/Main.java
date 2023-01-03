00ae9ebbc8350490a3ae7c80d990533dbf787ab9
eMThe94 <marek.t1994@interia.pl>
2023-01-03 19:48:44 +0100

    Rebase Main "dupa" na "lupa"

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -5,7 +5,7 @@ package pl.sdacademy;
  */
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello jędrna dupa!");
+    System.out.println("Hello jędrna lupa!");
 
   }
 }
